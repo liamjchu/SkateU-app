cf0961c90bed760c2610d6267130d9a1214192a8
liamjchu <liamchu0414@gmail.com>
2026-05-23 12:54:08 -0400

    added some map stuff, but it doesn't load

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,29 @@
-import { Text, View } from "react-native";
-import "../../global.css";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-4xl font-bold text-white">
-        TEST
-      </Text>
+    <View style={styles.container}>
+      <MapView
+        liteMode
+        style={styles.map}
+        initialRegion={{
+          latitude: 50.7128,
+          longitude: -74.006,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  map: {
+    flex: 1,
+  },
+});
