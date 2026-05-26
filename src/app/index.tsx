df58d97f671e935fd1b9daf14f483863b59e9c68
liamjchu <liamchu0414@gmail.com>
2026-05-26 13:36:48 -0400

    got maps to show up and working on zoom features

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,94 @@
-import { StyleSheet, View } from "react-native";
-import MapView from "react-native-maps";
+import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
+export default function Index() {
   return (
-    <View style={styles.container}>
-      <MapView
-        liteMode
-        style={styles.map}
-        initialRegion={{
-          latitude: 50.7128,
-          longitude: -74.006,
-          latitudeDelta: 0.05,
-          longitudeDelta: 0.05,
+    <View style={{ flex: 1 }}>
+      <WebView
+        source={{
+          html: `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+  />
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+
+  <style>
+    html, body {
+      margin: 0;
+      padding: 0;
+      background: #0b0f14;
+    }
+
+    #map {
+      height: 100vh;
+      width: 100vw;
+    }
+
+    /* Dark popup styling */
+    .leaflet-popup-content-wrapper {
+      background: #111827;
+      color: white;
+      border-radius: 12px;
+    }
+
+    .leaflet-popup-tip {
+      background: #111827;
+    }
+
+    /* Hide attribution for cleaner UI (optional) */
+    .leaflet-control-attribution {
+      display: none;
+    }
+  </style>
+</head>
+
+<body>
+  <div id="map"></div>
+
+  <script>
+  //start location  
+  var map = L.map('map', {
+      zoomControl: false
+    }).setView([41.8268, -71.4010], 15.5);
+    
+  // map style
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+  subdomains: 'abcd',
+}).addTo(map);
+
+// Limit scrolling area
+var bounds = L.latLngBounds(
+  [41.8068, -71.3810],
+  [41.8468, -71.4210]
+);
+
+//limit zoom area
+map.on('zoomend', function () {
+  if (map.getZoom() < 14.5) {
+    map.setZoom(14.5);
+  }
+  if (map.getZoom() > 100) {
+    map.setZoom(100);
+  }
+});
+
+map.setMaxBounds(bounds);
+map.on('drag', function () {
+  map.panInsideBounds(bounds, { animate: false });
+});
+
+  </script>
+</body>
+</html>
+          `
         }}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-  map: {
-    flex: 1,
-  },
-});
